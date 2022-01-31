9a19cb64a06fe86d2d4ee9d1035c0ba82f8cd3e5
ryansevidal <89810484+ryansevidal@users.noreply.github.com>
2022-01-30 22:07:56 -0500

    Flix Part 2
    added collection view and details screen

---
+++ Flixter/MovieGridViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridViewCell.swift
+//  Flixter
+//
+//  Created by Ryan Sevidal on 1/30/22.
+//
+
+import UIKit
+
+class MovieGridViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flixter/ViewController.swift
+++ Flixter/ViewController.swift
@@ -33,7 +33,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                  self.movies = dataDictionary["results"] as! [[String:Any]]
                  
                  self.tableView.reloadData()
-                 
                  print(dataDictionary)
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -67,6 +66,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for:cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
     }
 
 
